9df6dfea6ff9e92909e0d37c2d577bea9ffa92a4
LeVaNmInH1703 <105649952+LeVaNmInH1703@users.noreply.github.com>
2023-10-24 00:36:06 +0700

    linh tinh

--- smplayer/PlayerUI/FormPlayVideo.cs
+++ smplayer/PlayerUI/FormPlayVideo.cs
@@ -12,6 +12,7 @@ namespace PlayerUI
 {
     public partial class FormPlayVideo : Form
     {
+
         public FormPlayVideo()
         {
             InitializeComponent();
@@ -20,9 +21,9 @@ namespace PlayerUI
 
         private void trackList_SelectedIndexChanged(object sender, EventArgs e)
         {
-            timer1.Start();
+            /*timer1.Start();
             player.URL = paths[trackList.SelectedIndex];
-            player.Ctlcontrols.play();
+            player.Ctlcontrols.play();*/
         }
 
         private void btnPlayVideo_Click(object sender, EventArgs e)
@@ -39,29 +40,69 @@ namespace PlayerUI
         {
             if (player.playState == WMPLib.WMPPlayState.wmppsPlaying)
             {
-                progressBar1.Maximum = (int)(player.Ctlcontrols.currentItem.duration * 100000);
-                progressBar1.Value=(int)(player.Ctlcontrols.currentPosition*100000);
+                player.fullScreen = true;
+                /*progressBar1.Maximum = (int)(player.Ctlcontrols.currentItem.duration * 100000);
+                progressBar1.Value = (int)(player.Ctlcontrols.currentPosition * 100000);
                 label1.Text = player.Ctlcontrols.currentPositionString;
-                label2.Text=player.Ctlcontrols.currentItem.durationString;
+                label2.Text = player.Ctlcontrols.currentItem.durationString;*/
             }
         }
 
         private void trackBar1_Scroll(object sender, EventArgs e)
         {
-            player.settings.volume=trackBar1.Value;
-            player.Ctlcontrols.currentPosition = 0;
+            /*player.settings.volume=trackBar1.Value;
+            player.Ctlcontrols.currentPosition = 0;*/
+        }
+
+        private void FormPlayVideo_Load(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Multiselect = true;
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                files = ofd.SafeFileNames;
+                paths = ofd.FileNames;
+            }
+            player.uiMode = "None";
+            
+            player.URL = paths[0];
+            player.Ctlcontrols.play();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            player.fullScreen = player.fullScreen^true;
+        }
+
+        private void player_MouseMoveEvent(object sender, AxWMPLib._WMPOCXEvents_MouseMoveEvent e)
+        {
+            player.enableContextMenu = false;
+            
+        }
+        int HOPGB = 12;/*Hight of progress bar*/
+        private void progressBar1_MouseEnter(object sender, EventArgs e)
+        {
+            progressBar1.Size = new Size(progressBar1.Width, HOPGB);
+            progressBar1.Location= new Point(progressBar1.Location.X, progressBar1.Location.Y- HOPGB/2);
+        }
+
+        private void progressBar1_MouseLeave(object sender, EventArgs e)
+        {
+            progressBar1.Size = new Size(progressBar1.Width, 2);
+            progressBar1.Location = new Point(progressBar1.Location.X, progressBar1.Location.Y + HOPGB / 2);
         }
 
         private void btnOpen_Click(object sender, EventArgs e)
         {
-            OpenFileDialog ofd = new OpenFileDialog();
+            /*OpenFileDialog ofd = new OpenFileDialog();
             ofd.Multiselect = true;
             if(ofd.ShowDialog() == DialogResult.OK) { 
                 files= ofd.SafeFileNames;
                 paths = ofd.FileNames;
                 for(int i=0;i<files.Length; i++)
                     trackList.Items.Add(files[i]);
-            }
+            }*/
 
         }
     }
